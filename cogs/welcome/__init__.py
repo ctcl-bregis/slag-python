406b1f3b899d617380cbca805f01478318dc0b3e
ctcl-bregis <brayden.regis@gmail.com>
2024-01-29 20:01:46 -0500

    some sort of welcome messages

--- cogs/welcome/__init__.py
+++ cogs/welcome/__init__.py
@@ -1,10 +1,33 @@
 # SLAG - CTCL 2024
 # File: cogs/welcome.py
-# Purpose: Sends a welcome message on user join
+# Purpose: Sends a welcome message on user join or a leave message on user leave
 # Created: January 27, 2024
-# Modified: January 27, 2024
+# Modified: January 29, 2024
 
 from datetime import datetime
-from discord.ext import commmands
+from discord.ext import commands
 
-#class Welcome(commands.Cog):
+class Welcome(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    # TODO: Add configuration command or something that defines what channel in what guild to use to send these messages
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        embed = discord.Embed(title="Welcome {member.name}!", color=0x00ff00)
+        embed.set_thumbnail(url = member.display_avatar)
+
+        discord.TextChannel.send(discord.utils.get(ctx.guild.channels, name="welcome"), embed = embed)
+        
+
+    @commands.Cog.listener()
+    async def on_member_remove(self, member):
+        embed = discord.Embed(title="Member Left: {member.name}", color=0xff0000)
+        embed.set_thumbnail(url = member.display_avatar)  
+
+        discord.TextChannel.send(discord.utils.get(ctx.guild.channels, name="welcome"), embed = embed)
+   
+
+
+async def setup(client):
+    await client.add_cog(Welcome(client))
